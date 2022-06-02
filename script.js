eb0f46788917ea0f2f4a4463a9f910052252719d
erin-maxson <erin@cohatch.com>
2022-06-02 16:24:38 -0700

    updating index.html

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 var dateTimeEl = document.querySelector('#currentDay')
 
+//display the current date and time
+//display the hours between 8-5 on the current day | blocks change different colors if they are past, present, or future
+//click into the time box and add your calendar info
+//hit save to save your entry in the calendar
+
+
+
 // handle displaying the time
 function displayTime() {
     var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
     dateTimeEl.text(rightNow);
   }
+
