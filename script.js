e6035ed2a40ec814e6e96c54330504299f5bae9f
erin-maxson <erin@cohatch.com>
2022-06-02 15:51:11 -0700

    adding displayTime function

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+var dateTimeEl = document.querySelector('#currentDay')
+
 // handle displaying the time
 function displayTime() {
     var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-    timeDisplayEl.text(rightNow);
+    dateTimeEl.text(rightNow);
   }
