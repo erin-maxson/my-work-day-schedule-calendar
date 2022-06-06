1ad1f27a1e31f25bbf9471b47cde3aaf76fdeb4a
erin-maxson <erin@cohatch.com>
2022-06-06 15:55:05 -0700

    adding ids to timeblocks, creating displayTimeColor

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
-
-// creates variable for current day ID on index.html and logs it to the console
-// function: same as doing document.querySelector("#currentDay")
-var currentDay = $("#currentDay")
-console.log(currentDay) 
-
-var systemTime = moment().format("dddd, MMMM Do")
-// function: same as doing currentDay.textContent = moment()
-currentDay.text(systemTime)
-
-var currentHour = moment().hour()
-console.log(currentHour)
-
-var dateTimeEl = document.querySelector('#currentDay');
-
+// PSUEDO CODE below:
 // display the current date and time
 // display the hours between 9-5 on the current day
+// translate the time blocks to military time
 // compare the time blocks with moment to change different colors if they are past, present, or future
 // if moment() is > timeblocks => past
 // if moment() is == to timeblock => present
@@ -22,4 +9,31 @@ var dateTimeEl = document.querySelector('#currentDay');
 // click into the time box and add your calendar info, grab textarea value and store it in local storage
 
 
-// handle displaying the time
+// creates variable for current day ID on index.html and logs it to the console
+// function: same as doing document.querySelector("#currentDay")
+var currentDay = $("#currentDay")
+console.log(currentDay) 
+
+// variable that formats the date correctly
+var systemTime = moment().format("dddd, MMMM Do")
+
+// adds the current day to the webpage using the systemTime variable
+// function: same as doing currentDay.textContent = moment()
+currentDay.text(systemTime)
+
+// variable timeBlockArr creates an array to translate current time format to military time
+var timeBlockArr = [9,10,11,12,13,14,15,16,17]
+
+// formats the time shown into hours
+var currentHour = moment().hour()
+// logs the hours into the console
+console.log(currentHour)
+
+function displayTimeColor() {
+    for (var i = 0; i < timeBlockArr.length; i++) {
+        //this if would be past
+        if (currentHour > timeBlockArr[i]) {
+            $("#" + timeBlockArr[i])
+        }
+    }
+}
